9c994230147fffcbc25d9b9b8124c75dbf129744
Nicolas Larrosa <nicolaslarrosa.mail@gmail.com>
2023-07-22 10:16:22 -0300

    Template inicial para la clase 2

--- src/02-Clase-Repaso/ProductGalery.jsx
+++ src/02-Clase-Repaso/ProductGalery.jsx
@@ -9,32 +9,22 @@ export const ProductGalery = () => {
   return (
     <>
         <div className="container px-4 py-5" id="featured-3">
-            <h2 className="pb-2 border-bottom text-white">Ultimas Novedades</h2>
-            <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
-                <div className="feature col">
+            <h2 className="pb-2 border-bottom text-white">MIS TAREAS</h2>
+            <div className="row g-4 py-5 row-cols-1">
+                <div className="feature col border-bottom pb-3">
+                    <h5 className='text-warning'>Tarea N°: 1</h5>
                     <h3 className="fs-2 text-white">Featured title</h3>
-                    <p className="text-white">Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <button className="btn btn-primary icon-link">
-                    Ver info
-                    <svg className="bi"><use xlink:href="#chevron-right"/></svg>
+                    <button type='button' className="btn btn-sm btn-outline-danger">
+                    Eliminar
                     </button>
                 </div>
-                <div className="feature col">
-                    <h3 className="fs-2 text-white">Featured title</h3>
-                    <p className="text-white">Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <button className="btn btn-primary icon-link">
-                    Ver info
-                    <svg className="bi"><use xlink:href="#chevron-right"/></svg>
-                    </button>
+                <div class="mb-3 col-5">
+                    <input type="text" class="form-control"  placeholder="Agregar una tarea"/>
                 </div>
-                <div className="feature col">
-                    <h3 className="fs-2 text-white">Featured title</h3>
-                    <p className="text-white">Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                    <button className="btn btn-primary icon-link">
-                    Ver info
-                    <svg className="bi"><use xlink:href="#chevron-right"/></svg>
-                    </button>
+                <div class="mb-3 col-5">
+                    <button className='btn btn-success'>AGREGAR</button>
                 </div>
+
             </div>
         </div>
         
