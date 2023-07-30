42c83418bbf0eed89fcd91b6c7a44173de15ce62
Nicolas Larrosa <nicolaslarrosa.mail@gmail.com>
2023-07-30 01:25:21 -0300

    Se agrego buscador y categorias alGifMoji

--- src/03-Clase-Repaso/Gimoji.jsx
+++ src/03-Clase-Repaso/Gimoji.jsx
@@ -9,7 +9,7 @@ export const Gimoji = () => {
                 <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <div className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                         <a href="#" className="navbar-brand d-flex align-items-center">
-                            <strong>Album</strong>
+                            <strong>GifMoji</strong>
                         </a>
                     </div>
 
@@ -24,23 +24,25 @@ export const Gimoji = () => {
         <section className="py-5 text-center container text-white">
             <div className="row py-lg-5">
             <div className="col-lg-6 col-md-8 mx-auto">
-                <h1 className="fw-light">Album example</h1>
-                <p className="lead text-white">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+                <h1 className="fw-light">Los Mejores Gif/Emoji</h1>
+                <p className="lead text-white">Busca y copia y pega los mejores gif y emojis para compartir en tus redes o proyectos.</p>
             </div>
             </div>
         </section>
 
-        <div class="row">
-            <div className="col-sm-4">
-                <select className="form-select" aria-label="Default select example">
-                    <option selected>Selecciona una Categoria</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
-            </div>
-            <div className="col-sm-6">
-            <input type="search" className=" form-control form-control-dark text-dark" placeholder="Search..." aria-label="Search"/>
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-4">
+                    <select className="form-select" aria-label="Default select example">
+                        <option defaultValue={true}>Selecciona una Categoria</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+                <div className="col-sm-6">
+                <input type="search" className=" form-control form-control-dark text-dark" placeholder="Search..." aria-label="Search"/>
+                </div>
             </div>
         </div>
 
@@ -51,43 +53,40 @@ export const Gimoji = () => {
                     <div className="card shadow-sm">
                         <img src="" alt="Girl in a jacket" width={'100%'} height={'230'}/>
                         <div className="card-body">
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <div className="d-flex justify-content-between align-items-center">
                             <div className="btn-group">
-                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">Copy</button>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">Favoritos</button>
                             </div>
-                            <small className="text-body-secondary">9 mins</small>
+                            <small className="text-body-secondary">cod: 12546</small>
                         </div>
                         </div>
                     </div>
                     </div>
                     <div className="col">
                     <div className="card shadow-sm">
-                        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                        <img src="" alt="Girl in a jacket" width={'100%'} height={'230'}/>
                         <div className="card-body">
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <div className="d-flex justify-content-between align-items-center">
                             <div className="btn-group">
-                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">Copy</button>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">Favoritos</button>
                             </div>
-                            <small className="text-body-secondary">9 mins</small>
+                            <small className="text-body-secondary">cod: 12546</small>
                         </div>
                         </div>
                     </div>
                     </div>
                     <div className="col">
                     <div className="card shadow-sm">
-                        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                        <img src="" alt="Girl in a jacket" width={'100%'} height={'230'}/>
                         <div className="card-body">
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <div className="d-flex justify-content-between align-items-center">
                             <div className="btn-group">
-                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">Copy</button>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">Favoritos</button>
                             </div>
-                            <small className="text-body-secondary">9 mins</small>
+                            <small className="text-body-secondary">cod: 12546</small>
                         </div>
                         </div>
                     </div>
